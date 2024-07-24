0dfb6362db1ce8ffe85a9d51c6279250d3eaeeaf
Aaron Vienneau <aaron.vienneau@gmail.com>
2024-07-24 19:31:16 -0400

    docs(dynamic_table.js): add copyright notice
    add copyright notice
    
    closes utp-10

--- cypress/e2e/dynamic_table.cy.js
+++ cypress/e2e/dynamic_table.cy.js
@@ -1,3 +1,10 @@
+/**
+ * © Aaron Daniel Vienneau-Herring, 2024. All rights reserved.
+ * This software and its associated files are the property of Aaron Daniel Vienneau-Herring.
+ * Unauthorized copying, distribution, reproduction, or any other use of this software or its associated files, in whole or in part, is strictly prohibited
+ * without the prior written permission of Aaron Daniel Vienneau-Herring.
+ **/
+
 Cypress._.times(5, () => {
   describe("the dynamic table app", () => {
     beforeEach(() => {
