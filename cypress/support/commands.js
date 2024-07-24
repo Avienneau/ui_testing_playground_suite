f6dd1bd2c3e9a0901bec65e4ea952f628dd85e7a
Aaron Vienneau <aaron.vienneau@gmail.com>
2024-07-24 19:33:31 -0400

    feat(commands.js): add generic function for matching capturing text
    add generic function for matching and capturing text via regex
    
    closes utp-11

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,32 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/**
+ * © Aaron Daniel Vienneau-Herring, 2024. All rights reserved.
+ * This software and its associated files are the property of Aaron Daniel Vienneau-Herring.
+ * Unauthorized copying, distribution, reproduction, or any other use of this software or its associated files, in whole or in part, is strictly prohibited
+ * without the prior written permission of Aaron Daniel Vienneau-Herring.
+ **/
+
+/**
+ * match_and_capture_textreturns a cypress promise that, when invoked,
+ * 1. finds a selector
+ * 2. extracts its text
+ * 3. normalizes the captured text
+ * 4. compares it to a regular expression, potentially capturing data
+ * 5. checks for a capture
+ * 6. returns the capture at the specified index
+ **/
+Cypress.Commands.add(
+  "match_and_capture_text",
+  (regularExpression, selector, captureIndex = 0) => {
+    return cy
+      .get(selector)
+      .invoke("text")
+      .then((text) => {
+        const trimmed_text = text.trim();
+        const result = regularExpression.exec(trimmed_text);
+        if (result) {
+          return result[captureIndex];
+        }
+        return "";
+      });
+  },
+);
