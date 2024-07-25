5ccd1071050bc391426ef3ef70ed60d895cd77a7
Aaron Vienneau <aaron.vienneau@gmail.com>
2024-07-24 21:02:25 -0400

    docs(test): testtest
    testetstes

--- cypress/e2e/dynamic_table.cy.js
+++ cypress/e2e/dynamic_table.cy.js
@@ -2,7 +2,7 @@
  * © Aaron Daniel Vienneau-Herring, 2024. All rights reserved.
  * This software and its associated files are the property of Aaron Daniel Vienneau-Herring.
  * Unauthorized copying, distribution, reproduction, or any other use of this software or its associated files, in whole or in part, is strictly prohibited
- * without the prior written permission of Aaron Daniel Vienneau-Herring.
+ * without the prior written permission of Aaron Daniel Vienneau-Herring
  **/
 
 Cypress._.times(5, () => {
